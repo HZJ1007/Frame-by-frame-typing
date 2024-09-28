0fd6c80875768d71de679e5fc48c76ab4f5b5d37
Hong Byte <158826416+HZJ1007@users.noreply.github.com>
2024-09-28 11:55:59 +0800

    Add files via upload

---
+++ 逐帧打字术.cpp
@@ -0,0 +1,47 @@
+// Suggest not to change the code randomly.
+// This program only supports output of uppercase and lowercase letters and spaces 
+// User Customize
+#define outstr "Hello World" //Change the output string.
+#define wait 1 //Set the wait time, 0 if no wait. 
+
+
+
+
+
+//Code 
+#include <iostream>
+#include<cstdlib>
+#include <string>
+#include<ctime>
+#include <windows.h>
+using namespace std;
+const string s=outstr;
+char fun(void)
+{
+	int rd=rand()%53;
+	if (rd==52)
+		return ' ';
+	if (rd>=0 and rd<=25)
+		return rd+'A';
+	else
+		return rd-26+'a';
+}
+int main()
+{
+	
+	srand(time(0));
+	for (int i=0;i<s.size();i++)
+	{
+		int o;
+		do
+		{
+			o=fun();
+			for (int j=0;j<i;j++)
+				printf("%c",s[j]);
+			printf("%c\n",o);
+			Sleep(wait);
+		} while (o!=s[i]);
+		
+	}
+	return 0;
+}
